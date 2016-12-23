c0662ae0b7bfa6fdba1e74bdafa1584d70a29fd1
Konstantinos Papadimitriou <k.s.papadimitriou@gmail.com>
2016-12-23 15:21:18 +0200

    Update per_work.sh

--- results/workload_vm_12.12/per_work/per_work.sh
+++ results/workload_vm_12.12/per_work/per_work.sh
@@ -2,12 +2,26 @@
 
 avg(){
 	average=0
+	local i=0
 	for i in $@
 	do
-		average=$(($average + $i))
+		average=`echo -e "scale=2; ($average + $i)" | bc -l`
 	done
 	average=`echo -e "scale=2; ($average / $#)" | bc -l`
 }
 
-avg $@
-echo -e $average
+for work in */; do
+	cd $work;
+	echo $work
+	for sched in */; do
+		echo $sched
+		for i in {1..8}; do
+			bench=`ls $sched/$i*`
+			# echo -e "$i - $bench"
+			av=`grep "real" $sched/$i*/$bench | awk {'print $2'}`
+			avg $av
+			echo -e "\t$i-$bench\t$average"
+		done
+	done > test.txt
+	cd ..
+done
